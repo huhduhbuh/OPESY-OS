cc70ce669de7818992139d5725c4cdf15e410f95
diegoh123 <diego_martin_herrera@dlsu.edu.ph>
2024-09-10 14:27:43 +0800

    main loop added

--- main.cpp
+++ main.cpp
@@ -19,6 +19,22 @@ void setColor(int colorCode) {
     cout << "\033[" << colorCode << "m";
 }
 
+// for reprinting the header after clearing the screen
+void printHeader() {
+        cout << R"(  ____ ____   ___  ____  _____ ______   __
+ / ___/ ___| / _ \|  _ \| ____/ ___\ \ / /
+| |   \___ \| | | | |_) |  _| \___ \\ V / 
+| |___ ___) | |_| |  __/| |___ ___) || |  
+ \____|____/ \___/|_|   |_____|____/ |_|)";
+
+    setColor(32);
+    cout << "\nHello, Welcome to CSOPESY commandline!\n";
+    setColor(33);
+    cout << "Type 'exit' to quit, 'clear' to clear the screen\n";
+    setColor(37);
+    cout << "Enter a command: ";
+}
+
 int main() {
     clearScreen();
     cout << R"(  ____ ____   ___  ____  _____ ______   __
@@ -34,9 +50,39 @@ int main() {
     setColor(37);
     cout << "Enter a command: ";
 
+    bool run = true; // flag for running or ending loop
     string input;
-    getline(cin, input);
-    cout << input;
+    
+    // loop for command line choices
+    while (run) {
+        getline(cin, input);
+
+        if (input == "initialize") {
+            cout << "Initialize command recognized. Doing something.\n";
+            cout << "Enter a command: "; // replace with actual command logic later
+        } else if (input == "screen") {
+            cout << "Screen command recognized. Doing something.\n";
+            cout << "Enter a command: "; // replace with actual command logic later
+        } else if (input == "scheduler-test") {
+            cout << "Scheduler-test command recognized. Doing something.\n";
+            cout << "Enter a command: "; // replace with actual command logic later
+        } else if (input == "scheduler-stop") {
+            cout << "Scheduler-stop command recognized. Doing something.\n";
+            cout << "Enter a command: "; // replace with actual command logic later
+        } else if (input == "report-util") {
+            cout << "Report-util command recognized. Doing something.\n";
+            cout << "Enter a command: "; // replace with actual command logic later
+        } else if (input == "clear") {
+            clearScreen();
+            printHeader();
+        } else if (input == "exit") {
+            cout << "Exit command recognized. Exiting program.\n";
+            run = false;
+        } else {
+            cout << "Command not recognized. Please try again.\n";
+            cout << "Enter a command: ";
+        }
+    }
 
     return 0;
 }
