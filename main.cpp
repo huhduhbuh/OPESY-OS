811268938e7b7a011464d8f0881b876215897560
diegoh123 <diego_martin_herrera@dlsu.edu.ph>
2024-09-25 17:50:15 +0800

    added screen -s functionality

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
 #include <cstdlib>
 #include <cstring>
+#include <ctime>
+#include <map>
+#include <iomanip>
 using namespace std;
+
+// struct used for each new instance of a process screen
+struct ProcessScreen {
+    string processName;
+    int currentLine;
+    int totalLines;
+    string timeStamp;
+};
+
+// function to get local time stamp
+string getTimeStamp() {
+    time_t now = time(0);
+    tm *ltm = localtime(&now);
+    char buffer[80];
+    strftime(buffer, 80, "%m/%d/%Y, %I:%M:%S %p", ltm);
+
+    return string(buffer);
+}
+
+map<string, ProcessScreen> processScreens; // map used for storing process screens, uses process name as key
 
 void clearScreen() {
     #ifdef _WIN32
@@ -33,11 +56,27 @@ void printHeader() {
     cout << "Enter a command: ";
 }
 
+// function for displaying new process screen information after screen -s is entered
+// note: the const string& basically allows the process name to still be referenced for screen -r
+// ^^ im assuming thought thats how it works lol
+void displayScreen(const string& processName) {
+    if (processScreens.find(processName) != processScreens.end()) {
+        clearScreen();
+
+        ProcessScreen& ps = processScreens[processName];
+        cout << "Process: " << ps.processName << endl;
+        cout << "Instructions: " << ps.currentLine << "/" << ps.totalLines << endl;
+        cout << "Screen created at: " << ps.timeStamp << endl;
+    } else {
+        cout << "Process '" << processName << "' not found.\n";
+    }
+}
+
 int main() {
     printHeader();
-
     bool run = true; // flag for running or ending loop
     string input;
+    string currentScreen = "";
     
     // loop for command line choices
     while (run) {
@@ -46,9 +85,12 @@ int main() {
         if (input == "initialize") {
             cout << "Initialize command recognized. Doing something.\n";
             cout << "Enter a command: "; // replace with actual command logic later
-        } else if (input == "screen") {
-            cout << "Screen command recognized. Doing something.\n";
-            cout << "Enter a command: "; // replace with actual command logic later
+        } else if (input.find("screen -s") == 0) {
+            string processName = input.substr(10);
+            ProcessScreen newScreen = {processName, 0, 100, getTimeStamp()};
+            processScreens[processName] = newScreen;
+            currentScreen = processName;
+            displayScreen(processName);
         } else if (input == "scheduler-test") {
             cout << "Scheduler-test command recognized. Doing something.\n";
             cout << "Enter a command: "; // replace with actual command logic later
@@ -62,8 +104,12 @@ int main() {
             clearScreen();
             printHeader();
         } else if (input == "exit") {
-            cout << "Exit command recognized. Exiting program.\n";
-            run = false;
+            if (!currentScreen.empty()) {
+                currentScreen = "";
+                printHeader();
+            } else {
+                run = false;
+            }
         } else {
             cout << "Command not recognized. Please try again.\n";
             cout << "Enter a command: ";
